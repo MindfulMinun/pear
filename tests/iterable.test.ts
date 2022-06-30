99db8d726985ba1f3e0dc6a68fd15fe0253148ca
MindfulMinun <benji@benjic.xyz>
2022-06-30 01:18:15 -0500

    Fix tests for iterable

--- tests/iterable.test.ts
+++ tests/iterable.test.ts
@@ -29,10 +29,18 @@ Deno.test("range: one arg", () => {
     assert(iter.next().done)
 })
 
-
 Deno.test("range: two args", () => {
-    const iter = range(50_000)
-    for (let i = 0; i < 50_000; i++) {
+    const iter = range(40, 50)
+    for (let i = 40; i < 50; i++) {
+        const next = iter.next()
+        assertEquals(next.value, i)
+    }
+    assert(iter.next().done)
+})
+
+Deno.test("range: three args", () => {
+    const iter = range(60, 80, 4)
+    for (let i = 60; i < 80; i += 4) {
         const next = iter.next()
         assertEquals(next.value, i)
     }
